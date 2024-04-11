a95e867a732fe8f3827d07ef589866e9a0d2ad06
Futurizm <zantrebatyrbek@gmail.com>
2024-04-11 11:01:55 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Привязка позиции мыши к поозиции нашей сцены
+document.addEventListener('mousemove', e => {
+    Object.assign(document.documentElement, {
+        style: `
+        --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+        --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+        `
+    })
+})
